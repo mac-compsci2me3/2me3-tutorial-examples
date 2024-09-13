09f48828883e0234e3c6d562970fb53d87392383
Eden Nalian <122690785+EdenNal@users.noreply.github.com>
2024-09-13 19:37:54 -0400

    this is my first commit

--- src/firstExamples/InputExample.java
+++ src/firstExamples/InputExample.java
@@ -9,15 +9,15 @@ import java.util.*;
  */
 public class InputExample {
    public static void main(String[] args) {
-      Scanner in = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
 
       // get first input
       System.out.print("What is your name? ");
-      String name = in.nextLine();
+      String name = scanner.nextLine();
 
       // get second input - demonstrate the type conversion error
       System.out.print("How old are you? ");
-      int age = in.nextInt();
+      int age = scanner.nextInt();
 
       // display output on console
       System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
