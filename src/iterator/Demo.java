54cc35b6b22e679d7315fb6dc0bbedd45f00cfe1
Irene Y. Yuan <ireneyeyuan@gmail.com>
2024-10-19 11:48:13 -0400

    Update design patterns related code

---
+++ src/iterator/Demo.java
@@ -0,0 +1,13 @@
+package iterator;
+
+public class Demo {
+	
+   public static void main(String[] args) {
+      NameRepository namesRepository = new NameRepository();
+
+      for(Iterator iter = namesRepository.getIterator(); iter.hasNext();){
+         String name = (String)iter.next();
+         System.out.println("Name : " + name);
+      } 	
+   }
+}
